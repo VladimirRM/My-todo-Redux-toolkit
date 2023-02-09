f5a043fbcf1e9ed568ba87c8cca1ac82264a2d05
Vladimir RM <shvdrummer@gmail.com>
2023-02-09 19:59:10 +0200

    create TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { removeTodo } from './todoSlice'
 
-const TodoItem = ({todo}) => {
+const TodoItem = ({todo,text}) => {
+    const dispatch = useDispatch()
+
+    const deleteTodoHandler =(id)=>{
+        dispatch(removeTodo(id))
+    }
   return (
     <div>
-        {}
+        {todo.text}
+        <button onClick={()=>deleteTodoHandler(todo.id)}>Delete</button>
     </div>
   )
 }
--- src/components/todoSlice.js
+++ src/components/todoSlice.js
@@ -1,21 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos:[]
-}
+  todos: [],
+};
 
 const todoSlice = createSlice({
-    name:'todos',
-    initialState,
-    reducers:{
-        addTodo:(state,action)=>{
-            state.todos.push(action.payload)
-        },
-        removeTodo:(state,action)=>{
-            state.todos = state.todos.filter(todo=>todo.id !==action.payload)
-        }
-    }
-})
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
 
-export const {addTodo,removeTodo}= todoSlice.actions
-export default todoSlice.reducer
+export const { addTodo, removeTodo } = todoSlice.actions;
+export default todoSlice.reducer;
