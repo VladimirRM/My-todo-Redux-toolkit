c59f309a6940e0e7dcf86fc499a4267aef8885f2
Vladimir RM <shvdrummer@gmail.com>
2023-02-09 18:09:10 +0200

    create const dispatch

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css'
 import './index.css'
 import Form from './components/Form'
 import TodoItem from './components/TodoItem'
+import { useSelector } from 'react-redux'
+
 const App = () => {
+
+  const todos = useSelector((state)=>state.todo.todos)
   return (
     <div>
       <Form/>
-      <TodoItem/>
+      {todos.map(todo=>(
+      <TodoItem
+      todo={todo}
+      key={todo.id}/>
+
+      ))}
     </div>
   )
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,12 @@
 import React,{useState} from 'react'
+import { addTodo } from './todoSlice'
+import { useDispatch } from 'react-redux'
+
 
 const Form = () => {
+    
+
+    const dispatch = useDispatch()
   return (
     <div>Form</div>
   )
