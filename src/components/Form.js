72f431c5f77586b8c5318395a6136f89c903dc17
Vladimir RM <shvdrummer@gmail.com>
2023-02-09 15:23:24 +0200

    add button add

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,27 @@
 import React,{useState} from 'react'
+import { useDispatch } from 'react-redux'
+import { addTodo } from './todoSlice'
 
-const Form = () => {
+const Form = ({todo}) => {
+    const [value,setValue]=useState('')
+     const dispatch =useDispatch()
+
+     const addTodo = ()=>{
+        const addHandlerTodo = {
+            id:Math.random(),
+            text: value,
+        }
+        dispatch(addTodo(todo))
+        setValue('')
+     }
+
   return (
     <div>
-      
+      <input type="text"
+      value={value}
+      onChange={(e=>setValue(e.target.value))}
+      />
+      <button>Add</button>
     </div>
   )
 }
