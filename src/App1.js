c7d3d20faaaf71c01e3524566a6fc298206081d4
Vladimir RM <shvdrummer@gmail.com>
2023-02-09 20:03:21 +0200

    create store

---
+++ src/App1.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import "./index.css";
+import Form from "./components/Form";
+import TodoItem from "./components/TodoItem";
+import { useSelector } from "react-redux";
+
+const App = () => {
+  const todos = useSelector((state) => state.todo.todos);
+  return (
+    <div>
+      <Form />
+      {todos.map((todo) => (
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoSlice from "../components/todoSlice";
+
 
 export const store = configureStore({
-  reducer: {
-    todo: todoSlice,
-  },
-});
+    reducer:{
+        todo: todoSlice
+    }
+})
