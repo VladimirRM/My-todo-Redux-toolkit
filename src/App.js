7d7383b01775807a877aacc05ba97159475db471
Vladimir RM <shvdrummer@gmail.com>
2023-02-09 17:52:00 +0200

    add const initialState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import "./App.css";
-import Form from "./components/Form";
-import TodoItem from "./components/TodoItem";
-import { useSelector } from "react-redux";
-function App() {
-  const todos = useSelector((state) => state.todo.todos);
+import React from 'react'
+import './App.css'
+
+const App = () => {
   return (
-    <div className="App">
-      <Form />
-      {todos.map((todo) => (
-        <TodoItem todo={todo} 
-        key={todo.id}/>
-      ))}
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
