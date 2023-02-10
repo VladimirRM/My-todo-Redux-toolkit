1a9954ca9a993ea21d9e2b6fcb0620af24bc51d2
Vladimir RM <shvdrummer@gmail.com>
2023-02-10 11:36:43 +0200

    create Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,30 @@
-import React,{useState} from 'react'
-import { useDispatch } from 'react-redux'
-import { addTodo } from './TodoItem'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "./TodoItem";
 
 const Form = () => {
+  const [value, setValue] = useState("");
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-     const addTodoHandler = ()=>{
-        const todo = {
-            id:Math.random(),
-            text:value,
-        }
-     }
+  const addTodoHandler = () => {
+    const todo = {
+      id: Math.random(),
+      text: value,
+    };
+    dispatch(addTodo(todo));
+    setValue("");
+  };
   return (
     <div>
-      
+      <input
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      <button opnClick={()=>addTodoHandler()}>Add todo</button>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
