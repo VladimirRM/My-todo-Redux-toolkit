e60bb7a75c3ba65401b7d99a6b92d36fcc427052
Vladimir RM <shvdrummer@gmail.com>
2023-02-10 11:11:44 +0200

    create components TodoItem

---
+++ src/components/TodoItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { removeTodo } from './todoSlice'
+
+const TodoItem = ({todo}) => {
+
+    const dispatch = useDispatch()
+
+    const removeTodoHandler =(id)=>{
+        dispatch(removeTodo(id))
+    }
+  return (
+    <div>
+
+        {todo.text}
+        <button onClick={()=>removeTodoHandler()}>Delete</button>
+    </div>
+  )
+}
+
+export default TodoItem
