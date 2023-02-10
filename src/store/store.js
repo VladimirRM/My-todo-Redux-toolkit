4c28e3d6036564ab34e103d41417f9dec3e59238
Vladimir RM <shvdrummer@gmail.com>
2023-02-10 10:52:06 +0200

    clean code

--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todoSlice from "../components/todoSlice";
-export const store = configureStore({
-  reducer: {
-    todo: todoSlice,
-  },
-});
