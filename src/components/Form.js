51d0861f6d799a90b74c8edf0bca619a778bb625
Vladimir RM <shvdrummer@gmail.com>
2023-02-10 11:34:04 +0200

    import useState

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useDispatch } from 'react-redux'
 import { addTodo } from './TodoItem'
 
 const Form = () => {
 
-    const dispatch 
+    const dispatch = useDispatch()
+
+     const addTodoHandler = ()=>{
+        const todo = {
+            id:Math.random(),
+            text:value,
+        }
+     }
   return (
     <div>
       
