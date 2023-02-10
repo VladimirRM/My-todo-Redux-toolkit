5c8dbe50f3c147c79ed5ca0f75552796f612c263
Vladimir RM <shvdrummer@gmail.com>
2023-02-10 10:56:26 +0200

    create  reducer addTodo

--- src/components/todoSlice.js
+++ src/components/todoSlice.js
@@ -4,4 +4,14 @@ const initialState = {
     todos:[],
 }
 
+const todoSlice = createSlice({
+    name:' todos',
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+            state.todos.push(action.payload)
+        },
+    }
+})
+
 
